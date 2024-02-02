19c10e49919f121c3bd43bb79f8d76705e08eae8
MD-Suhad <ssuhad490@gmail.com>
2024-02-02 16:33:47 +0600

    class toppic finished

--- class/BasicClass.js
+++ class/BasicClass.js
@@ -32,14 +32,52 @@ const lions = class LionsSectionRed{
     this.student = " Learning";
   }
 };
-console.log(this.teacher, this.student);
+//console.log(this.teacher, this.student);
+
+// const obj = {
+//   proto: {
+//     prop:"foo",
+//   },
+//   notAMethod: function(){
+//     console.log(obj.prop);
+//   }
+// }
+
+// const objName = {
+
+//   name:"shohaib",
+//   family(){
+//     return this.name;
+//   },
+
+// };
+// console.log(objName.name);
 
 
+class Point {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
 
+  static displayName = "Point";
+  static distance(a, b) {
+    const dx = a.x - b.x;
+    const dy = a.y - b.y;
 
+    return Math.hypot(dx, dy);
+  }
+}
 
+const p1 = new Point(5, 5);
+const p2 = new Point(10, 10);
+p1.displayName; // undefined
+p1.distance; // undefined
+p2.displayName; // undefined
+p2.distance; // undefined
 
-
+console.log(Point.displayName); // "Point"
+console.log(Point.distance(p1, p2)); // 7.0710678118654755
 
 
 
