5d01ac05cd737f2afbf72d5e6f1f2cb0011a7b78
MD-Suhad <ssuhad490@gmail.com>
2024-02-03 21:41:01 +0600

    1 solution added

--- Async/tempCodeRunnerFile.js
+++ Async/tempCodeRunnerFile.js
@@ -2,4 +2,4 @@ let promise = new Promise(function (resolve, reject) {
 //   //this promise function is the executor function
 //   let value = "water";
 //   resolve(value);
-// });
+});
---
+++ class/getClass.js
@@ -0,0 +1,16 @@
+// const obj = {
+//    log: ['a', 'b', 'c'],
+//    get latest() {
+//      return this.log[this.log.length - 1];
+//    },
+//  };
+ 
+//  console.log(obj.latest);
+// const obj = {
+//    log: ["example", "test"],
+//    get latest() {
+//      if (this.log.length === 0) return undefined;
+//      return this.log[this.log.length - 1];
+//    },
+//  };
+//  console.log(obj.latest)
