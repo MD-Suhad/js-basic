922a7ba2130356704cf4453ff5e3d3f870f07895
MD-Suhad <ssuhad490@gmail.com>
2024-02-16 21:28:40 +0600

    problem updated

--- Js 30 Days Problem/Promise & Time/ExcuteAsynchronous.js
+++ Js 30 Days Problem/Promise & Time/ExcuteAsynchronous.js
@@ -1,34 +1,34 @@
-class EventEmitter {
+// class EventEmitter {
     
-   /**
-    * @param {string} eventName
-    * @param {Function} callback
-    * @return {Object}
-    */
-   subscribe(eventName, callback) {
-       if(!this.eventMap.hasOwnProperty(eventName)){
-         this.eventMap[eventName] = new Set();
-       }
-       this.eventMap[eventName].add(callback);
-       return {
-           unsubscribe: () => {
+//    /**
+//     * @param {string} eventName
+//     * @param {Function} callback
+//     * @return {Object}
+//     */
+//    subscribe(eventName, callback) {
+//        if(!this.eventMap.hasOwnProperty(eventName)){
+//          this.eventMap[eventName] = new Set();
+//        }
+//        this.eventMap[eventName].add(callback);
+//        return {
+//            unsubscribe: () => {
 
-               this.eventMap[eventName].delete(callback);
+//                this.eventMap[eventName].delete(callback);
                
-           }
-       };
-   }
+//            }
+//        };
+//    }
    
-   /**
-    * @param {string} eventName
-    * @param {Array} args
-    * @return {Array}
-    */
-   emit(eventName, args = []) {
-       const res = [];
-       (this.eventMap[eventName] ?? [])
-         .forEach((callback) => res.push(callback(...args)));
-       return res;
+//    /**
+//     * @param {string} eventName
+//     * @param {Array} args
+//     * @return {Array}
+//     */
+//    emit(eventName, args = []) {
+//        const res = [];
+//        (this.eventMap[eventName] ?? [])
+//          .forEach((callback) => res.push(callback(...args)));
+//        return res;
        
-   }
-}
+//    }
+// }
