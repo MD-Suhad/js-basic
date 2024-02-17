2b05ff8aeaee31ef62afd2d590a9430921e8bc47
MD-Suhad <ssuhad490@gmail.com>
2024-02-17 10:28:17 +0600

    js problem solve added

--- Js 30 Days Problem/Promise & Time/ExcuteAsynchronous.js
+++ Js 30 Days Problem/Promise & Time/ExcuteAsynchronous.js
@@ -1,34 +1,15 @@
-// class EventEmitter {
-    
-//    /**
-//     * @param {string} eventName
-//     * @param {Function} callback
-//     * @return {Object}
-//     */
-//    subscribe(eventName, callback) {
-//        if(!this.eventMap.hasOwnProperty(eventName)){
-//          this.eventMap[eventName] = new Set();
-//        }
-//        this.eventMap[eventName].add(callback);
-//        return {
-//            unsubscribe: () => {
-
-//                this.eventMap[eventName].delete(callback);
-               
-//            }
-//        };
-//    }
-   
-//    /**
-//     * @param {string} eventName
-//     * @param {Array} args
-//     * @return {Array}
-//     */
-//    emit(eventName, args = []) {
-//        const res = [];
-//        (this.eventMap[eventName] ?? [])
-//          .forEach((callback) => res.push(callback(...args)));
-//        return res;
-       
-//    }
-// }
+var promiseAll = async function(functions){
+   return new Promise((res,rej) =>{
+      let len = functions.length, ct = 0;
+      const ans = new Array();
+      for(let i = 0; i < len; ++i){
+         functions[ind]()
+         .then((val) => {
+            ans[ind] = val;
+            ct++;
+            if(ct === len)
+            res(ans);
+         }) .catch(err => rej(err))
+      }
+   })
+}
