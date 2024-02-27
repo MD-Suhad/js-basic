6f48480b0418518f9fa99878cb4c3446688b3c71
MD-Suhad <ssuhad490@gmail.com>
2024-02-27 22:52:27 +0600

    problem updated

---
+++ InterviewProblem/palindrome.js
@@ -0,0 +1,5 @@
+function isPalindrome(str){
+   const cleanStr = str.replace(/[^a-zA-Z0-9]/g, ”).toLowerCase(); 
+   const reversedStr = cleanStr.split(").reverse().join(");
+   return clearStr === reversedStr;
+}
