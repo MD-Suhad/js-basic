b7c9063759f2cc30da692907e02098fd31398553
MD-Suhad <ssuhad490@gmail.com>
2024-03-16 23:11:14 +0600

    another solve added

--- InterviewProblem/10_Problems_JS/reducedMethod.js
+++ InterviewProblem/10_Problems_JS/reducedMethod.js
@@ -1,17 +1,34 @@
 // using reduce method of array
 
 let arr = ['hello','world','java','hello','java','shohaib','suhad','suhad'];
-function countWord(p) {
-   let result = p.reduce((allNames,name) =>{
-      if(name in allNames){
-         allNames[name]++;
+// function countWord(p) {
+//    let result = p.reduce((allNames,name) =>{
+//       if(name in allNames){
+//          allNames[name]++;
+//       }
+//       else{
+//          allNames[name] = 1;
+//       }
+//       return allNames;
+//    },{});
+//    return result;
+// }
+
+function countWord(p){
+   let count = {};
+   p.forEach((item) =>{
+      if(count[item]){
+         count[item]++; 
+
       }
       else{
-         allNames[name] = 1;
+         count[item] = 1;
       }
-      return allNames;
-   },{});
-   return result;
+   })
+   return count;
 }
+
+
+
 
 console.log(countWord(arr));
