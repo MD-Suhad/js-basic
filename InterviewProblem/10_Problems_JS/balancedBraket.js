1804786cb530b0e44e228d71c4c983504797a1be
MD-Suhad <ssuhad490@gmail.com>
2024-03-19 23:04:10 +0600

    solve added

--- InterviewProblem/10_Problems_JS/balancedBraket.js
+++ InterviewProblem/10_Problems_JS/balancedBraket.js
@@ -1,1 +1,21 @@
-const isValid = (str)
+const isValid = (str) => {
+   let s = str.replace(/\s+/g,'');
+   if(s.length % 2 !== 0) return false;
+
+   const stack = [];
+   const map = new Map([
+      ['(', ')', ],
+      ['[', ']', ],
+      ['{', '}']
+   ])
+   for(let i = 0; i < s.length; i+=1){
+      if(map.has(s[i])){
+         stack.push(map.get(s[i]));
+      }
+      else if(s[i] !== stack.pop()){
+         return false;
+      }
+   }
+   return stack.length === 0;
+};
+
